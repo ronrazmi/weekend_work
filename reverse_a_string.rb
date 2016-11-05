2987b4acd293eeeb9614719eb76ee4cad725c6fe
ronrazmi <ronrazmi@gmail.com>
2016-11-05 13:03:55 -0400

    Add files via upload

---
+++ reverse_a_string.rb
@@ -0,0 +1,25 @@
+# Complete the method called reverse_a_string that accepts a string as a parameter and 
+# returns the reverse. The one caveat: Don't use the reverse method that already
+# comes with Ruby!
+
+def reverse_a_string(string)
+	letter_array = string.split ("")
+	reverse_letter_array = letter_array.reverse
+	reverse_letter_array.join
+end
+
+# Driver code - don't touch anything below this line.
+puts "TESTING reverse_a_string..."
+puts
+
+result = reverse_a_string("abcde")
+
+puts "Your method returned:"
+puts result
+puts
+
+if result == "edcba"
+  puts "PASS!"
+else
+  puts "F"
+end
